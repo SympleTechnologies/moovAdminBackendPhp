cf00eaee054c849482d41aa5e95ba877dcba658b
israelalagbe <israelalagbe53@gmail.com>
2019-03-03 19:17:38 +0100

    Fixed variables not found

--- v2/src/app/Controllers/Booking.php
+++ v2/src/app/Controllers/Booking.php
@@ -71,9 +71,9 @@ class Booking extends Api_Controller {
 
 			->first();
 
-		/*$peekHours = $this->db->table('ride_peak_hours')
+		$peekHours = $this->db->table('ride_peak_hours')
 
-			->select("rph_from as start", "rph_to as to")->get()->toArray();*/
+			->select("rph_from as start", "rph_to as to")->get()->toArray();
 
 // print_r(collect($peekHours)->toArray());
 
