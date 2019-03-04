8ad62b35d1e378b3bb14c43c8df8cd57142f57ae
israelalagbe <israelalagbe53@gmail.com>
2019-03-04 11:32:27 +0100

    Added access control

--- v2/index.php
+++ v2/index.php
@@ -1,10 +1,11 @@
 <?php
-use \Psr\Http\Message\ServerRequestInterface as Request;
 
 require '../vendor/autoload.php';
 // date_default_timezone_set ('Asia/Kolkata'); //Africa/Lagos
- date_default_timezone_set('Africa/Lagos'); //Africa/Lagos
-
+date_default_timezone_set('Africa/Lagos'); //Africa/Lagos
+header("Access-Control-Allow-Origin:*");
+header("Access-Control-Allow-Methods:GET,POST,PUT,DELETE,PATCH");
+/*header("Access-Control-Allow-Headers:Content-Type,X-XSRF-TOKEN,token");*/
 $settings = require 'src/config/settings.php';
 $app = new \Slim\App($settings);
 require '../jfunctions.php';
@@ -14,21 +15,20 @@ require 'src/dependencies.php';
 require 'src/config/Environment.php';
 $amw = new src\app\Middleware\JAuth();
 
-
 require 'src/AdminRoutes.php';
 require 'src/SchoolRoutes.php';
 require 'src/routes.php';
 
 $app->options('/{routes:.+}', function ($request, $response, $args) {
-    return $response;
+	return $response;
 });
 
 $app->add(function ($req, $res, $next) {
-    $response = $next($req, $res);
-    return $response
-            ->withHeader('Access-Control-Allow-Origin', '*')
-            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization, Token')
-            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+	$response = $next($req, $res);
+	return $response
+		->withHeader('Access-Control-Allow-Origin', '*')
+		->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization, Token')
+		->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
 });
 
 $app->run();
