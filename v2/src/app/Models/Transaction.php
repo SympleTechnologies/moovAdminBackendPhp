4870f6c6a9281a03a4fac1d3d98af74a312a7ae0
israelalagbe <israelalagbe53@gmail.com>
2019-03-05 15:40:10 +0100

    Did a little fix on add driver and transaction model

--- v2/src/app/Models/Transaction.php
+++ v2/src/app/Models/Transaction.php
@@ -6,10 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 class Transaction extends Model {
 	protected $primaryKey = 't_id';
 	protected $table = 'transactions';
-	protected $fillable = [];
+	//protected $fillable = [];
+	//mark id this as non fillable
+	protected $guarded = ['id'];
 	const CREATED_AT = 't_started';
 	const UPDATED_AT = 't_date';
-//public $timestamps = false;
+	public $timestamps = true;
 
 	public function user() {
 		return $this->belongsTo('src\app\Models\User', 't_user_id');
