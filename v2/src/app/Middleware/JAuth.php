eae01169da7d7e4b13367684305063d4945f8119
israelalagbe <israelalagbe53@gmail.com>
2019-03-05 16:10:41 +0100

    Testing auth token

--- v2/src/app/Middleware/JAuth.php
+++ v2/src/app/Middleware/JAuth.php
@@ -5,80 +5,75 @@ use mukto90\Ncrypt;
 use src\app\Models\User as Users;
 use src\config\Api_Controller;
 
-class JAuth extends Api_Controller
-{
+class JAuth extends Api_Controller {
 
-    public function __construct()
-    {
-        global $app;
-        $this->slim = $app;
-        $this->app = $app->getContainer();
-        // $this->args=$app->args();
-        $this->db = $this->app['db'];
-        $this->request = $this->app['request'];
-        $this->response = $this->app['response'];
-        $this->logger = $this->app['logger'];
-        $this->env = $this->app['environment'];
-        $this->view = $this->app['view'];
-        $this->uri = $this->request->getUri();
-        $this->input = $this->request->getParsedBody();
-        // parent::__construct();
-        $this->ncrypt = new Ncrypt();
-        $this->ncrypt->set_secret_key($this->env['oauth_secret_key']);
-        $this->ncrypt->set_secret_iv($this->env['oauth_secret_iv']);
-        $this->ncrypt->set_cipher('AES-256-CBC'); //optional
+	public function __construct() {
+		global $app;
+		$this->slim = $app;
+		$this->app = $app->getContainer();
+		// $this->args=$app->args();
+		$this->db = $this->app['db'];
+		$this->request = $this->app['request'];
+		$this->response = $this->app['response'];
+		$this->logger = $this->app['logger'];
+		$this->env = $this->app['environment'];
+		$this->view = $this->app['view'];
+		$this->uri = $this->request->getUri();
+		$this->input = $this->request->getParsedBody();
+		// parent::__construct();
+		$this->ncrypt = new Ncrypt();
+		$this->ncrypt->set_secret_key($this->env['oauth_secret_key']);
+		$this->ncrypt->set_secret_iv($this->env['oauth_secret_iv']);
+		$this->ncrypt->set_cipher('AES-256-CBC'); //optional
 
-        // c url  $this->request->getUri()->getPath();
+		// c url  $this->request->getUri()->getPath();
 
-    }
+	}
 
-    public function __invoke($request, $response, $next)
-    {
-        ($this->key_check()) ? '' : die($this->key_failed());
-        $response = $next($request, $response);
-        //after
-        return $response;
-    }
-    public function key_check()
-    {
-        $headers = apache_request_headers();
-        $token = $headers['token'];
-        $tokenstring = $this->ncrypt->decrypt($token);
-        $tokendata = explode(",", unserialize($tokenstring));
-        if (!empty($tokendata)) {
-            $useridFromtoken = $tokendata[0];
-            $useridFromTime = $tokendata[1];
+	public function __invoke($request, $response, $next) {
+		($this->key_check()) ? '' : die($this->key_failed());
+		$response = $next($request, $response);
+		//after
+		return $response;
+	}
+	public function key_check() {
+		$headers = apache_request_headers();
+		$token = $headers['token'];
+		$tokenstring = $this->ncrypt->decrypt($token);
+		$tokendata = explode(",", unserialize($tokenstring));
+		if (!empty($tokendata)) {
+			$useridFromtoken = @$tokendata[0];
+			$useridFromTime = @$tokendata[1];
 
-            $userdata = array(
-                'u_id' => $useridFromtoken,
-                'u_token' => $token,
-            );
-            $count = Users::where($userdata)->count();
+			$userdata = array(
+				'u_id' => $useridFromtoken,
+				'u_token' => $token,
+			);
+			$count = Users::where($userdata)->count();
 
-            if ($count > 0) {
-                return true;
+			if ($count > 0) {
+				return true;
 
-            } else {
-                return false;
-            }
-        } else {
-            return false;
-        }
+			} else {
+				return false;
+			}
+		} else {
+			return false;
+		}
 
-    }
+	}
 
-    public function key_failed()
-    {
-        $headers = apache_request_headers();
-        $token = $headers['token'];
+	public function key_failed() {
+		$headers = getallheaders();
+		var_dump(getallheaders());
+		$token = $headers['token'];
+		$output = array(
+			"status" => false,
+			"data" => array(),
+			"message" => "access token failed",
+			"token_received" => $token,
+		);
+		return $this->response->withJson($output)->withStatus(401);
 
-        $output = array(
-            "status" => false,
-            "data" => array(),
-            "message" => "access token failed",
-            "token_received" => $token,
-        );
-        return $this->response->withJson($output)->withStatus(401);
-
-    }
+	}
 }
