95e57748360be538fd928b360e1493ab0f4989ac
seunkoko <seunowonikoko@gmail.com>
2019-03-06 16:41:23 +0100

    Remove .env from github repo

--- v2/src/config/env.php
+++ v2/src/config/env.php
@@ -1,25 +1,6 @@
 <?php
 $ENVIRONMENT_VARIABLES = [
 
-	// 'DB_NAME' => 'themoova_pp_test',
-	// 'DB_USER' => 'root',
-	// 'DB_PASS' => '',
-	// 'DB_HOST' => 'localhost',
-
-	//Heroku config
-	/*'DB_NAME' => 'heroku_701b649672bdddd',
-	'DB_USER' => 'b1c916ec2507c8',
-	'DB_PASS' => '1f2d801d',
-	'DB_HOST' => 'us-cdbr-iron-east-03.cleardb.net',
-
-	'MAIL_DRIVER' => 'log'
-	, 'MAIL_HOST' => 'smtp.gmail.com'
-	, 'MAIL_PORT' => 587
-	, 'MAIL_USERNAME' => 'israelalagbe53@gmail.com'
-	, 'MAIL_PASSWORD' => 'Superbru'
-	, 'MAIL_ENCRYPTION' => 'tls'
-	, 'MAIL_PRETEND' => true,*/
-
 ];
 if (file_exists('.env')) {
 	$ENVIRONMENT_VARIABLES = array_merge(parse_ini_file(".env"), $ENVIRONMENT_VARIABLES);
