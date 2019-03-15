7aaef52a0bd0df790c3353546a8c3f04d0fe68d9
israelalagbe <israelalagbe53@gmail.com>
2019-03-15 19:33:52 +0100

    Validated registration fields

--- v2/src/app/Controllers/Auth.php
+++ v2/src/app/Controllers/Auth.php
@@ -1,5 +1,6 @@
 <?php
 namespace src\app\Controllers;
+
 use mukto90\Ncrypt;
 use Slim\Http\UploadedFile;
 use src\app\Helpers\Hwallet;
@@ -11,15 +12,20 @@ use src\app\Models\UserLoginLog;
 use src\app\Models\Wallet;
 use src\config\Api_Controller;
 use src\config\Mail_Controller;
-class Auth extends Api_Controller {
-	public function __construct($app) {
+use Rakit\Validation\Validator;
+
+class Auth extends Api_Controller
+{
+	public function __construct($app)
+	{
 		parent::__construct();
 		$this->ncrypt = new Ncrypt();
 		$this->ncrypt->set_secret_key($this->env['oauth_secret_key']);
 		$this->ncrypt->set_secret_iv($this->env['oauth_secret_iv']);
 		$this->ncrypt->set_cipher('AES-256-CBC'); //optional
 	}
-	public function login() {
+	public function login()
+	{
 		$links = array(
 			"self" => $this->self,
 			"forgot_password" => base_url($this->env['route']['user']['forgot']),
@@ -83,7 +89,8 @@ class Auth extends Api_Controller {
 		}
 		return $this->response->withJson($output);
 	}
-	public function social_login() {
+	public function social_login()
+	{
 		$links = array(
 			"self" => $this->self,
 			"forgot_password" => base_url($this->env['route']['user']['forgot']),
@@ -133,7 +140,7 @@ class Auth extends Api_Controller {
 			// $userlogs->ull_login_time = time_now();
 			// $userlogs->ull_logout_time = time_now();
 			// $userlogs->save();
-			
+
 			$output = array(
 				"status" => true,
 				"data" => array(
@@ -159,7 +166,8 @@ class Auth extends Api_Controller {
 		}
 		return $this->response->withJson($output);
 	}
-	public function save_url_img($img_url) {
+	public function save_url_img($img_url)
+	{
 		$dir = $this->app->get('profile_pic_upload');
 		$filename = randomPassword(10);
 		$url = $img_url;
@@ -172,7 +180,58 @@ class Auth extends Api_Controller {
 		fclose($fp);
 		return $filename . ".png";
 	}
-	public function register() {
+	public function validateRegistration()
+	{
+		$path = $this->request->getUri()->getPath();
+		$validator = new Validator([
+			'required' => ':attribute field is required',
+			'email' => ':email field is required',
+			'record_exists' => ":attribute doesn't match any user record"
+		]);
+		if ($path == 'auth/social_registration') {
+			//Social Registration
+
+			$validation = $validator->make($this->input, [
+				"f_name" => "required",
+				"l_name" => "required",
+				"email" => "required|email",
+				"college" => "required|numeric",
+				"phone" => "required|numeric",
+				"user_type" => "required|numeric",
+				"phone_country" => 'required|regex:/^\+\d+?$/',
+				"gender" => "required|in:male,female",
+				"auth_mode" => "required|in:social",
+				"auth_provider" => "required|in:google,facebook",
+				"auth_uid" => "required",
+				"device_type" => "required|in:android,iOS",
+				"push_token" => "required",
+				"app_version" => "required",
+				"device_id" => "required"
+			]);
+		} else {
+			//Email Registration
+			$validation = $validator->make($this->input, [
+				"f_name" => "required",
+				"l_name" => "required",
+				"email" => "required|email",
+				"password" => "required",
+				"college" => "required|numeric",
+				"phone" => "required|numeric",
+				"user_type" => "required|numeric",
+				"phone_country" => 'required|regex:/^\+\d+?$/',
+				"gender" => "required|in:male,female",
+				"auth_mode" => "required|in:email",
+				"device_type" => "required|in:android,iOS",
+				"push_token" => "required",
+				"app_version" => "required",
+				"device_id" => "required"
+			]);
+		}
+		$validation->validate();
+		return $validation;
+	}
+	public function register()
+	{
 		$connection = $this->db->getConnection();
 		$connection->beginTransaction();
 		$links = array(
@@ -180,105 +239,108 @@ class Auth extends Api_Controller {
 			"login" => base_url($this->env['route']['user']['login']),
 			"forgot_password" => base_url($this->env['route']['user']['forgot']),
 		);
-		$required = array('f_name', 'l_name', 'email', 'password', 'college', 'phone');
-		$missing = array_diff_key(array_flip($required), $this->input);
-		/*$postText = file_get_contents('php://input');
-			var_dump($postText);
-		*/
-		if (empty($missing)) {
-			$email = $this->input['email'];
-			$count = Users::where('u_email', $email)->count();
-			if ($count <= 0) {
-				if (empty($this->input['password'])) {
-					$userpassword = randomPassword();
-				} else {
-					$userpassword = $this->input['password'];
-				}
-				
-				$gender = strtolower($this->input['gender']);
-				
-				$filename = $gender . '.png';
-				//  $filename = $gender . '.png';
-				$links = array(
-					"self" => $this->self,
-					"forgot_password" => base_url($this->env['route']['user']['forgot']),
-					"register" => base_url($this->env['route']['user']['register']),
-				);
-				//$method = $this->input['provider'];
-				//$socailid = $this->input['uid'];
-				//$deviceid = $this->input['device_id'];
-				$pushToken = $this->input['push_token'];
-				$deviceId = $this->input['device_id'];
-				$deviceType = $this->input['device_type'];
-				$appVersion = $this->input['app_version'];
-				$tokenstring = time_now();
-				$token = $this->ncrypt->encrypt(serialize($tokenstring));
-				$users = new Users;
-				$users->u_first_name = $this->input['f_name'];
-				$users->u_last_name = $this->input['l_name'];
-				$users->u_email = $this->input['email'];
-				$users->u_password = $userpassword;
-				$users->u_edu_institution = $this->input['college'];
-				$users->u_phone = $this->input['phone'];
-				$users->u_gender = $gender;
-				$users->u_auth_mode = $this->input['auth_mode'];
-				$users->u_auth_provider = $this->input['auth_provider'];
-				$users->u_auth_uid = $this->input['auth_uid'];
-				$users->u_type = $this->input['user_type'];
-				$users->u_image_200=$users->u_image_100=$users->u_image="https://res.cloudinary.com/moov-api/image/upload/v1552051608/public/uploads/userpic/User.png";
-				$users->u_phone_country = $this->input['phone_country'];
-				$users->u_token = $token;
-				$users->u_push_token = $pushToken;
-				$users->u_device_type = $deviceType;
-				$users->u_app_version = $appVersion;
-				$users->u_device_id = $deviceId;
-				$users->save();
-				$wallet = new Wallet;
-				$wallet->w_user_id = $users->u_id;
-				$wallet->w_amount = 0;
-				$wallet->save();
-				$bd = new BankDetails;
-				$bd->bd_user_id = $users->u_id;
-				$bd->save();
-				
-				
-				$users = Users::select('u_id', 'u_first_name', 'u_image', 'u_image_100', 'u_image_200')->where('u_id', $users->u_id)->first();
-				$users['wallet_balance'] = array("balance" => "0");
-				$output = array(
-					"status" => true,
-					"data" => array("user_details" => $users,
-						"access_token" => $token,
-						"user_pic_url" => $users['u_image'],
-						"user_pic_url_100" => $users['u_image_100'],
-						"user_pic_url_200" => $users['u_image_200'],
-					),
-					"message" => "login success",
-					"links" => $links,
-				);
+
+		$validation = $this->validateRegistration();
+		if ($validation->fails()) {
+			$response = array(
+
+				"status" => false,
+
+				"message" => "Input Validation Error!",
+				'errors' => $validation->errors()->toArray(),
+
+				"links" => array(
+
+					"self" => $this->uri->getBaseUrl() . "" . $this->uri->getBasePath() . "/" . $this->uri->getPath(),
+
+				),
+
+			);
+			return $this->response->withJson($response);
+		}
+		$email = $this->input['email'];
+		$count = Users::where('u_email', $email)->count();
+		if ($count <= 0) {
+			if (empty($this->input['password'])) {
+				$userpassword = randomPassword();
 			} else {
-				$output = array(
-					"status" => false,
-					"message" => "Email already Exists",
-					"errors" => array("message" => array("already exist")),
-					"links" => $links,
-				);
+				$userpassword = $this->input['password'];
 			}
+
+			$gender = strtolower($this->input['gender']);
+
+			$filename = $gender . '.png';
+			//  $filename = $gender . '.png';
+			$links = array(
+				"self" => $this->self,
+				"forgot_password" => base_url($this->env['route']['user']['forgot']),
+				"register" => base_url($this->env['route']['user']['register']),
+			);
+			//$method = $this->input['provider'];
+			//$socailid = $this->input['uid'];
+			//$deviceid = $this->input['device_id'];
+			$pushToken = $this->input['push_token'];
+			$deviceId = $this->input['device_id'];
+			$deviceType = $this->input['device_type'];
+			$appVersion = $this->input['app_version'];
+			$tokenstring = time_now();
+			$token = $this->ncrypt->encrypt(serialize($tokenstring));
+			$users = new Users;
+			$users->u_first_name = $this->input['f_name'];
+			$users->u_last_name = $this->input['l_name'];
+			$users->u_email = $this->input['email'];
+			$users->u_password = $userpassword;
+			$users->u_edu_institution = $this->input['college'];
+			$users->u_phone = $this->input['phone'];
+			$users->u_gender = $gender;
+			$users->u_auth_mode = @$this->input['auth_mode'];
+			$users->u_auth_provider = @$this->input['auth_provider'];
+			$users->u_auth_uid = @$this->input['auth_uid'];
+			$users->u_type = @$this->input['user_type'];
+			$users->u_image_200 = $users->u_image_100 = $users->u_image = "https://res.cloudinary.com/moov-api/image/upload/v1552051608/public/uploads/userpic/User.png";
+			$users->u_phone_country = $this->input['phone_country'];
+			$users->u_token = $token;
+			$users->u_push_token = $pushToken;
+			$users->u_device_type = $deviceType;
+			$users->u_app_version = $appVersion;
+			$users->u_device_id = $deviceId;
+			$users->save();
+			$wallet = new Wallet;
+			$wallet->w_user_id = $users->u_id;
+			$wallet->w_amount = 0;
+			$wallet->save();
+			$bd = new BankDetails;
+			$bd->bd_user_id = $users->u_id;
+			$bd->save();
+
+
+			$users = Users::select('u_id', 'u_first_name', 'u_image', 'u_image_100', 'u_image_200')->where('u_id', $users->u_id)->first();
+			$users['wallet_balance'] = array("balance" => "0");
+			$output = array(
+				"status" => true,
+				"data" => array(
+					"user_details" => $users,
+					"access_token" => $token,
+					"user_pic_url" => $users['u_image'],
+					"user_pic_url_100" => $users['u_image_100'],
+					"user_pic_url_200" => $users['u_image_200'],
+				),
+				"message" => "login success",
+				"links" => $links,
+			);
 		} else {
-			$error = array();
-			foreach ($missing as $key => $val) {
-				$error[] = array("data" => $key . " is missing");
-			}
 			$output = array(
 				"status" => false,
-				"message" => "Please fill all the fields",
-				"errors" => array("message" => $error),
-				"data" => array("reg_details" => array()),
+				"message" => "Email already Exists",
+				"errors" => array("message" => array("already exist")),
+				"links" => $links,
 			);
 		}
 		$connection->commit();
 		return $this->response->withJson($output);
 	}
-	public function forgot_password() {
+	public function forgot_password()
+	{
 		$mail = new Mail_Controller();
 		if (isset($this->input['email'])) {
 			$useremail = $this->input['email'];
@@ -315,7 +377,8 @@ class Auth extends Api_Controller {
 					"status" => true,
 					"message" => "A new password will be send to you if mail exist in our server!", //
 				);
-			}} else {
+			}
+		} else {
 			$output = array(
 				"status" => false,
 				"message" => "Please fill email!", //
@@ -323,7 +386,8 @@ class Auth extends Api_Controller {
 		}
 		return $this->response->withJson($output);
 	}
-	public function get_edu_list() {
+	public function get_edu_list()
+	{
 		$links = array(
 			"self" => $this->self,
 			"login" => base_url($this->env['route']['user']['login']),
@@ -340,7 +404,8 @@ class Auth extends Api_Controller {
 		);
 		return $this->response->withJson($result);
 	}
-	public function get_usertype_list() {
+	public function get_usertype_list()
+	{
 		$links = array(
 			"self" => $this->self,
 			"login" => base_url($this->env['route']['user']['login']),
@@ -357,18 +422,20 @@ class Auth extends Api_Controller {
 		);
 		return $this->response->withJson($result);
 	}
-	public function activation_resend() {
-	}
-	public function delete_account() {
-	}
-	public function moveUploadedFile($directory, UploadedFile $uploadedFile) {
+	public function activation_resend()
+	{ }
+	public function delete_account()
+	{ }
+	public function moveUploadedFile($directory, UploadedFile $uploadedFile)
+	{
 		$extension = pathinfo($uploadedFile->getClientFilename(), PATHINFO_EXTENSION);
 		$basename = bin2hex(random_bytes(8)); // see http://php.net/manual/en/function.random-bytes.php
 		$filename = sprintf('%s.%0.8s', $basename, $extension);
 		$uploadedFile->moveTo($directory . DIRECTORY_SEPARATOR . $filename);
 		return $filename;
 	}
-	public function check_email() {
+	public function check_email()
+	{
 		if (isset($this->input['email'])) {
 			$useremail = $this->input['email'];
 			$userdata = array(
