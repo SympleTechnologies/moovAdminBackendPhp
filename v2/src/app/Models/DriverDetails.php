33b44e0fe535d63b9e9efe4a08ab3f58cbdcf61f
israelalagbe <israelalagbe53@gmail.com>
2019-04-01 17:51:06 +0100

    Removed the included dependencies and started marking full use of the class autoloader

--- v2/src/app/Models/DriverDetails.php
+++ v2/src/app/Models/DriverDetails.php
@@ -17,4 +17,11 @@ class DriverDetails extends Model
     // const UPDATED_AT = 'u_last_modified';
     public $timestamps = false;
 
+    public function user() {
+		return $this->belongsTo('src\app\Models\User', 'dd_driver_id');
+    }
+    public function bank_detail() {
+		return $this->hasOne('src\app\Models\BankDetails', 'bd_user_id');
+	}
+
 }
